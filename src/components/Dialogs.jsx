f6bcaef0d53e767d847aa0de2a10dd1bfec314ed
sukhorukov.mv <sukhorukov.mv@reliab.tech>
2020-03-05 22:57:56 +0300

    initialization: add code

---
+++ src/components/Dialogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Dialogs = () => {
+    return <div>
+        hello dialogs
+    </div>
+}
+
+export default Dialogs;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Profile = () => {
+    return <div>
+        hello profile        
+    </div>
+}
+
+export default Profile;
